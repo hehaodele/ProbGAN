fix bug: remove gradient from historial discrinimators

models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 # 32 x 32 ==============================================================================================================
 class multi_generator_32(nn.Module):
     """
@@ -129,9 +130,14 @@ class multi_discriminator_with_history(nn.Module):
         self.ds = nn.Conv2d(self.ndf * 4, num_discs, 4, 1, 0, bias=False)
         self.n_ds = num_discs
 
-        with torch.no_grad():
-            self.ds_hist_avg = nn.Conv2d(self.ndf * 4, num_discs, 4, 1, 0, bias=False)
-            self.len_hist = 1.0
+        # with torch.no_grad():
+        #     self.ds_hist_avg = nn.Conv2d(self.ndf * 4, num_discs, 4, 1, 0, bias=False)
+        #     self.len_hist = 1.0
+
+        self.ds_hist_avg = nn.Conv2d(self.ndf * 4, num_discs, 4, 1, 0, bias=False)
+        for param in self.ds_hist_avg.parameters():
+            param.requires_grad = False
+        self.len_hist = 1.0
 
         self.backbone = nn.Sequential(
             # input size is in_size x 32 x 32
@@ -306,9 +312,14 @@ class multi_discriminator_48_with_history(nn.Module):
         self.ds = nn.Conv2d(self.ndf * 8, num_discs, 3, 1, 0, bias=False)
         self.n_ds = num_discs
 
-        with torch.no_grad():
-            self.ds_hist_avg = nn.Conv2d(self.ndf * 8, num_discs, 3, 1, 0, bias=False)
-            self.len_hist = 1.0
+        # with torch.no_grad():
+        #     self.ds_hist_avg = nn.Conv2d(self.ndf * 8, num_discs, 3, 1, 0, bias=False)
+        #     self.len_hist = 1.0
+
+        self.ds_hist_avg = nn.Conv2d(self.ndf * 8, num_discs, 3, 1, 0, bias=False)
+        for param in self.ds_hist_avg.parameters():
+            param.requires_grad = False
+        self.len_hist = 1.0
 
         self.backbone = nn.Sequential(
             # input size is in_size x 48 x 48

train.py
@@ -3,7 +3,7 @@ from common_head import *
 from models import *
 from utils import *
 from train_utils import *
-from inception.inception_score_tf import get_inception_score
+# from inception.inception_score_tf import get_inception_score
 from easydict import EasyDict
 
 
@@ -28,7 +28,8 @@ def parse():
     """
     Paths
     """
-    parser.add_argument('--dataset', default='cifar', type=str, dest='dataset', help='dataset: [cifar10, stl10, imagenet]')
+    parser.add_argument('--dataset', default='cifar', type=str, dest='dataset',
+                        help='dataset: [cifar10, stl10, imagenet]')
     parser.add_argument('--save_dir', default='none', type=str, dest='save_dir', help='save_path')
 
     return parser.parse_args()
@@ -251,8 +252,8 @@ def train_net(G, D, args, config):
         tmp = grad_info(D.parameters())
         print('D grad l2-norm: {}, value max: {}'.format(tmp[0], tmp[1]))
 
-        nn.utils.clip_grad_norm_(parameters=G.parameters(),max_norm=100,norm_type=2)
-        nn.utils.clip_grad_norm_(parameters=D.parameters(),max_norm=500,norm_type=2)
+        nn.utils.clip_grad_norm_(parameters=G.parameters(), max_norm=100, norm_type=2)
+        nn.utils.clip_grad_norm_(parameters=D.parameters(), max_norm=500, norm_type=2)
 
         if (epoch + 1) % config.dump_ep == 0:
             batch_size = 100
@@ -265,7 +266,8 @@ def train_net(G, D, args, config):
                 x.append(G_result.detach().cpu().numpy())
             x = np.concatenate(x, axis=0)
             imgs = x
-            m, v = get_inception_score(images=imgs)
+            # m, v = get_inception_score(images=imgs)
+            m, v = 0
             # fid = get_fid_score(images=imgs)
             fid = 0
             print('Epoch {} Inception Score: mean {:.6f} std {:.6f}'.format(epoch + 1, m, v))
@@ -282,7 +284,6 @@ if __name__ == '__main__':
     print(args)
 
     config = EasyDict()
-
 
     """
     Number of Generator/Discriminator Monte-Carlo samples
@@ -308,7 +309,6 @@ if __name__ == '__main__':
     config.g_batch_size = 128
     config.base_lr = 0.0001
     config.beta1 = 0.5
-
 
     if args.dataset == 'cifar10':
         from datasets import get_cifar10

train_utils.py
@@ -157,7 +157,7 @@ def plot_result(G, fixed_noise, image_size, num_epoch, save_dir, fig_size=(10, 1
 
     for ax, img in zip(axes.flatten(), generate_images):
         ax.axis('off')
-        ax.set_adjustable('box-forced')
+        # ax.set_adjustable('box-forced')
         if is_gray:
             img = img.cpu().data.view(image_size, image_size).numpy()
             ax.imshow(img, cmap='gray', aspect='equal')
@@ -192,14 +192,16 @@ def print_log_2(epoch, epoches, iteration, iters, learning_rate,
 def save_checkpoint(state, filename='checkpoint'):
     torch.save(state, filename + '.pth.tar')
 
+
 def grad_info(parameters):
     total_norm = 0
     total_abs_max = 0
     for p in parameters:
-        param_norm = p.grad.data.norm(2)
-        total_norm += param_norm.item() ** 2
-        vmax = p.grad.data.abs().max().item()
-        if vmax > total_abs_max:
-            total_abs_max = vmax
+        if p.requires_grad:
+            param_norm = p.grad.data.norm(2)
+            total_norm += param_norm.item() ** 2
+            vmax = p.grad.data.abs().max().item()
+            if vmax > total_abs_max:
+                total_abs_max = vmax
     total_norm = total_norm ** (1. / 2)
     return total_norm, total_abs_max